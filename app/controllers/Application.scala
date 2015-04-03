c39e061c253e8e152928e28ff43e76053092ab2e
Jeremy Sarchet <jeremysarchet@gmail.com>
2015-04-03 14:11:05 -0700

    restructure application code

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,26 +4,25 @@ import play.api.mvc._
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
 
-object Application extends Controller {
+class BasicTimer {
+  var message = ""
+  def reset() {message = "Please give this timer a try!"}
+  def start() {message = "Timer started, now get to work!"}
+  def pause() {message = "Timer paused. Enjoy your break."}
+  def check = message
+}
 
-  val dateFormatter = DateTimeFormat.forPattern("HH:mm:ss")
-  val randomNumberGenerator = new scala.util.Random
+object Application extends Controller {
 
   def index = Action {
     //computation here
     Ok(views.html.index("Hi, let's dogfood! :-)"))
   }
 
-  class BasicTimer {
-    var message = "Please give this timer a try!"
-    def start() {message = "Timer started, now get to work!"}
-    def pause() {message = "Timer paused. Enjoy your break."}
-    def check = message
-  }
-
   val myTimer = new BasicTimer
 
   def kibbles = Action {
+    myTimer.reset()
     Ok(views.html.kibbles(myTimer.check))
   }
 
@@ -37,16 +36,23 @@ object Application extends Controller {
     Ok(views.html.kibbles(myTimer.check))
   }
 
+
+  val dateFormatter = DateTimeFormat.forPattern("HH:mm:ss")
+
   def time = Action {
     val time = new DateTime()
     Ok(views.html.time(dateFormatter.print(time)))
   }
+
+
+  val randomNumberGenerator = new scala.util.Random
 
   def jbs = Action {
     //Jeremy's page which displays a random number between 0 and 'randomCeiling'
     val randomCeiling = scala.math.pow(2, 16).toInt
     Ok(views.html.jbs(randomNumberGenerator.nextInt(randomCeiling).toString()))
   }
+
 
   // () parentheses
   // [] (square) brackets
